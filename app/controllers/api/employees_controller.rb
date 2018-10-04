bb14e91eecbf520a58531c9b2ca6d741ccf51bbc
babylee2002 <babylee2002@gmail.com>
2018-10-04 18:36:23 -0400

    employee api data start to show

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -2,5 +2,7 @@ class Api::EmployeesController < Api::BaseController
   respond_to :json
 
   def index
+    @employee = Employee.all
+    render json: "test"
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,25 +1,20 @@
 class Employee < ApplicationRecord
+  API_BASE_URL = "https://api.salesloft.com/v2"
   PAGE_SIZE = "2"
-  API_BASE_URL = "https://api.salesloft.com/v2/people?per_page=#{PAGE_SIZE}"
+  URL = "#{API_BASE_URL}/people?per_page=#{PAGE_SIZE}"
+  HEADERS = {"Authorization" => "Bearer #{ENV.fetch("SALESLOFT_API_KEY")}"}
 
   include HTTParty
   puts "hello"
 
   def get_api_data
-    response = HTTParty.get(API_BASE_URL, headers: headers)
-    data = response.data
-    puts JSON.pretty_generate(data)
+    response = HTTParty.get(URL, headers: HEADERS)
+    data = response["data"]
     return data
   end
 
   employee = Employee.new
-  employee.get_api_data
-
-  private
-
-  def headers
-    {
-      "Authorization" => "Bearer #{ENV.fetch("SALESLOFT_API_KEY")}",
-    }
+  employee.get_api_data.each do |record|
+    puts JSON.pretty_generate(record)
   end
 end
