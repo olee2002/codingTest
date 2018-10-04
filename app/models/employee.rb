cc2077e8b5ca45bd5814cea722c059ce1c8110ac
babylee2002 <babylee2002@gmail.com>
2018-10-04 18:07:22 -0400

    api call salesloft

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,25 @@
 class Employee < ApplicationRecord
+  PAGE_SIZE = "2"
+  API_BASE_URL = "https://api.salesloft.com/v2/people?per_page=#{PAGE_SIZE}"
 
-    
+  include HTTParty
+  puts "hello"
+
+  def get_api_data
+    response = HTTParty.get(API_BASE_URL, headers: headers)
+    data = response.data
+    puts JSON.pretty_generate(data)
+    return data
+  end
+
+  employee = Employee.new
+  employee.get_api_data
+
+  private
+
+  def headers
+    {
+      "Authorization" => "Bearer #{ENV.fetch("SALESLOFT_API_KEY")}",
+    }
+  end
 end
