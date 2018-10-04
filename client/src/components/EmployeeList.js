c80cc3e3d6c0c6e02c1e50235a6dc13b00201074
babylee2002 <babylee2002@gmail.com>
2018-10-04 18:44:45 -0400

    React component EmployeeList created

---
+++ client/src/components/EmployeeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default EmployeeList
