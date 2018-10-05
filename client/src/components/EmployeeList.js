fb4806a8f6280bd57897d2b6d7d55934f61b9ced
babylee2002 <babylee2002@gmail.com>
2018-10-04 20:41:53 -0400

    split out the employeesTable to a separate component

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -4,25 +4,14 @@ import styled from 'styled-components'
 
 import { getEmployees } from '../actions/AsyncActions'
 
+import EmployeesTable from './EmployeesTable'
+
 const Container = styled.div`
 display: flex;
 justify-content:center;
 align-items: center;
 flex-direction: column;
 margin: 20px;
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-}
-tr:nth-child(even) {
-    background-color: #e9e9e9;
-}
 button{
     width: 200px;
     height: 35px;
@@ -32,7 +21,6 @@ button{
     margin: 5px;
 }
 `
-
 
 class EmployeeList extends Component {
 
@@ -59,23 +47,7 @@ class EmployeeList extends Component {
                     <button>Email Letter Frequency</button>
                     <button>Duplicate Emails</button>
                 </div>
-                <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Title</th>
-                        <th>Email</th>
-                        <th>Notes</th>
-                    </tr>
-                    {employees.map(e =>
-                        < tr >
-                            <td>{e.id}</td>
-                            <td>{e.first_name} {e.last_name}</td>
-                            <td>{e.title}</td>
-                            <td>{e.email_address}</td>
-                            <td></td>
-                        </tr>)}
-                </table>
+                <EmployeesTable employees={employees} />
             </Container>
         )
     }
---
+++ client/src/components/EmployeesTable.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+display: flex;
+justify-content:center;
+align-items: center;
+flex-direction: column;
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even) {
+    background-color: #e9e9e9;
+}
+`
+
+const EmployeesTable = (props) => {
+    return (
+        <Container>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Title</th>
+                    <th>Email</th>
+                    <th>Notes</th>
+                </tr>
+                {props.employees.map(e =>
+                    < tr >
+                        <td>{e.id}</td>
+                        <td>{e.first_name} {e.last_name}</td>
+                        <td>{e.title}</td>
+                        <td>{e.email_address}</td>
+                        <td></td>
+                    </tr>)}
+            </table>
+        </Container>
+    )
+}
+
+export default EmployeesTable
